79971a00470395eb8eb42b1e11b3eeac5a894510
tlbn <tete.leticia@gmail.com>
2020-06-15 16:51:38 -0300

    MV design

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,9 +2,10 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
+
 
 import java.io.IOException;
 
@@ -13,11 +14,13 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-            Parent parent = loader.load();
+            ScrollPane scrollPane = loader.load();
+            scrollPane.setFitToHeight(true);
+            scrollPane.setFitToWidth(true);
 
-            Scene mainScene = new Scene(parent);
+            Scene mainScene = new Scene(scrollPane);
             primaryStage.setScene(mainScene);
-            primaryStage.setTitle("Organizador de Bilioteca");
+            primaryStage.setTitle("Library Organizer");
             primaryStage.show();
         } catch (IOException e) {
             e.printStackTrace();
