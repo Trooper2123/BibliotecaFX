fed9cd8dc0995a97ca9571481d8eda6af4b08f0c
tlbn <tete.leticia@gmail.com>
2020-06-17 15:00:55 -0300

    Subject form dialog design

--- src/gui/SubjectListController.java
+++ src/gui/SubjectListController.java
@@ -1,18 +1,27 @@
 package gui;
 
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Subject;
 import model.services.SubjectService;
 import sample.Main;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -33,14 +42,15 @@ public class SubjectListController implements Initializable {
     @FXML
     private Button btNew;
 
-    private ObservableList<Subject>obsList;
+    private ObservableList<Subject> obsList;
 
     @FXML
-    public void onBtNewAction(){
-        System.out.println("onBtNewAction");
+    public void onBtNewAction(ActionEvent event) {
+        Stage parentStage = Utils.currentStage(event);
+        createDialogForm("/gui/SubjectForm.fxml", parentStage);
     }
 
-    public void setSubjectService(SubjectService service){
+    public void setSubjectService(SubjectService service) {
         this.service = service;
     }
 
@@ -49,20 +59,39 @@ public class SubjectListController implements Initializable {
         initializeNode();
     }
 
-    private void initializeNode(){
+    private void initializeNode() {
         tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
-        tableColumnName.setCellValueFactory(new  PropertyValueFactory<>("name"));
+        tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
 
         Stage stage = (Stage) Main.getMainScene().getWindow();
         tableViewSubject.prefHeightProperty().bind(stage.heightProperty());
     }
 
-    public void updateTableView(){
-        if (service == null){
+    public void updateTableView() {
+        if (service == null) {
             throw new IllegalStateException("Service was null");
         }
         List<Subject> list = service.findAll();
         obsList = FXCollections.observableArrayList(list);
         tableViewSubject.setItems(obsList);
+    }
+
+    private void createDialogForm(String absolutName, Stage parentStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absolutName));
+            Pane pane = loader.load();
+
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Enter Subject data");
+            dialogStage.setScene(new Scene(pane));
+            dialogStage.setResizable(false);
+            dialogStage.initOwner(parentStage);
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.showAndWait();
+
+        } catch (IOException e) {
+            Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
+        }
+
     }
 }
